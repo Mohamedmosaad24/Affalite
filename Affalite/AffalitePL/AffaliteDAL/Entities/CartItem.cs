4828828f77df78b75b8fc4dc725c01c5423bec67
sohailawael1 <sohailawaelfouadeltobshy@gmail.com>
2026-03-29 19:07:03 +0200

    initial commit

---
+++ Affalite/AffalitePL/AffaliteDAL/Entities/CartItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AffaliteDAL.Entities
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Cart? Cart { get; set; }
+        public Product? Product { get; set; }
+    }
+}
