f8a8c2cf7e622f620d95d9823a41f1ce9750d905
islamsoliamn1 <islamelayat38@gmail.com>
2026-03-31 21:24:54 +0200

    add rest of end points

--- AffalitePL/Controllers/OrdersController.cs
+++ AffalitePL/Controllers/OrdersController.cs
@@ -1,6 +1,9 @@
 ﻿using AffaliteBL.DTOs.OrderDTOs;
 using AffaliteBL.IServices;
-using Microsoft.AspNetCore.Http;
+using AffaliteDAL.Entities;
+using AffaliteDAL.Entities.Enums;
+using AffaliteDAL.IRepo;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AffalitePL.Controllers
@@ -10,14 +13,68 @@ namespace AffalitePL.Controllers
     public class OrdersController : ControllerBase
     {
         private readonly IOrderService _orderService;
-        public OrdersController(IOrderService orderService) => _orderService = orderService;
+        private readonly IGenericRepository<Order> _orderRepo;
+        private readonly IMapper _mapper;
+
+   
+        public OrdersController(IOrderService orderService, IGenericRepository<Order> orderRepo, IMapper mapper)
+        {
+            _orderService = orderService;
+            _orderRepo = orderRepo;
+            _mapper = mapper;
+        }
+
 
         [HttpPost]
-        public IActionResult Create(OrderCreateDTO dto) => Ok(_orderService.CreateOrder(dto));
+        public IActionResult Create(OrderCreateDTO dto)
+        {
+            var result = _orderService.CreateOrder(dto);
+            return Ok(result);
+        }
+
 
         [HttpGet("{id}")]
-        public IActionResult GetById(int id) => Ok(_orderService.GetOrderById(id));
+        public IActionResult GetById(int id)
+        {
+            var order = _orderRepo.GetById(id);
+            if (order == null) return NotFound();
+            return Ok(_mapper.Map<OrderReadDTO>(order));
+        }
+
+     
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var orders = _orderRepo.GetAll();
+            return Ok(_mapper.Map<IEnumerable<OrderReadDTO>>(orders));
+        }
+
+     
+        [HttpGet("merchant/{merchantId}")]
+        public IActionResult GetByMerchant(int merchantId)
+        {
+            var orders = _orderRepo.GetAll().Where(o => o.MerchantId == merchantId);
+            return Ok(_mapper.Map<IEnumerable<OrderReadDTO>>(orders));
+        }
+
+        [HttpGet("affiliate/{affiliateId}")]
+        public IActionResult GetByAffiliate(int affiliateId)
+        {
+            var orders = _orderRepo.GetAll().Where(o => o.AffiliateId == affiliateId);
+            return Ok(_mapper.Map<IEnumerable<OrderReadDTO>>(orders));
+        }
+
+
+        [HttpPut("{id}/status")]
+        public IActionResult UpdateStatus(int id, [FromBody] OrderStatus status)
+        {
+            var order = _orderRepo.GetById(id);
+            if (order == null) return NotFound();
+
+            order.Status = status;
+            _orderRepo.Update(order);
+            _orderRepo.SaveChanges();
+            return NoContent();
+        }
     }
-
-
 }
